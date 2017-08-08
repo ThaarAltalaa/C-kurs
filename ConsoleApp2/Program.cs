943df562bd52d26212b053838aa7067b31ca1dd8
samer husien <samer husien>
2017-08-08 15:12:40 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flmin = float.MinValue;
+            float flmix = float.MaxValue;
+
+            Console.WriteLine("MinValue = {0} , MixValue = {1}" , flmin , flmix); 
+
+
+            Console.WriteLine("\nHELLO WORLD");
+
+        //سلاش باك وبعدها حرف ن بتنزل سطر لتحت)
+
+            Console.WriteLine("\nthaar");
+            Console.ReadKey();     
+        
+               
+        }
+    }
+}
